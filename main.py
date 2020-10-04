dce1f1d3f0c0820c62ee15dda9c15cf6d4cfe616
Akash Singh <akashsingh2313@gmail.com>
2020-10-04 22:36:35 +0530

    Corrected adding user

--- main.py
+++ main.py
@@ -9,23 +9,42 @@ app = Flask(__name__)
 cluster = MongoClient("mongodb+srv://akash:akash@cluster0.zqitl.mongodb.net/<dbname>?retryWrites=true&w=majority")
 
 db = cluster["dbname"]
-collection = db["copill"]
+collection = db["coll"]
 
 
-def checuser(name)
-    if collection.find({}).length==0 :
-        return True
-    else:
+def checuser(name):
+    if collection.find_one({"name": name}):
         return False
+    else:
+        return True
 
 
 @app.route("/addUser", methods=["POST"])
 def adduser():
     request_payload = request.get_json(force=True)
-    if checuser(request_payload["name"]) :
-        collection.insert_one({"name": request_payload["name"]})
+  #  print(checuser(request_payload["name"]))
+    if checuser(request_payload["name"]):
+        collection.insert_one({"name": request_payload["name"], "tasks": []})
+        return "added User"
+    else:
+        return "User exist"
 
 
+@app.route("/addTask", methods=["POST"])
+def addtask():
+    request_payload = request.get_json(force=True)
+    if checuser(request_payload["name"]):
+        return "No such User"
+    else:
+        myquery = {"name": request_payload["name"]}
+        newvalues = {"$push":
+                         {
+                             "tasks": request_payload["task"],
+                              "done": False
+                         }
+                     }
+        collection.update_one(myquery, newvalues)
+        return "Added Task"
 
 
 
