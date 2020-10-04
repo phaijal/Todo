4d4edd49a7292953d7066a7460341adb6ddf57a3
Akash Singh <akashsingh2313@gmail.com>
2020-10-05 00:05:25 +0530

    changed tasks array to dict

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from flask import Flask
-from flask import request
+from flask import request, jsonify
 import pymongo
 from pymongo import MongoClient
 import json
 from bson import json_util
+from bson import ObjectId
 
 app = Flask(__name__)
 cluster = MongoClient("mongodb+srv://akash:akash@cluster0.zqitl.mongodb.net/<dbname>?retryWrites=true&w=majority")
@@ -18,16 +19,24 @@ def checuser(name):
     else:
         return True
 
+class JSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
+
 
 @app.route("/addUser", methods=["POST"])
 def adduser():
     request_payload = request.get_json(force=True)
   #  print(checuser(request_payload["name"]))
     if checuser(request_payload["name"]):
-        collection.insert_one({"name": request_payload["name"], "tasks": []})
+        collection.insert_one({"name": request_payload["name"], "tasks": {}})
         return "added User"
     else:
         return "User exist"
+
+
 
 
 @app.route("/addTask", methods=["POST"])
@@ -36,18 +45,35 @@ def addtask():
     if checuser(request_payload["name"]):
         return "No such User"
     else:
+        task=request_payload["task"]
+        str = f"tasks.{task}"
         myquery = {"name": request_payload["name"]}
-        newvalues = {"$push":
+        newvalues = {"$set":
                          {
-                             "tasks": {
-                                        "task": request_payload["task"],
-                                        "done": False
-                             }
+                             str: False
                          }
                      }
         collection.update_one(myquery, newvalues)
         return "Added Task"
 
+
+@app.route("/getTask", methods=["POST"])
+def getTask():
+    request_payload = request.get_json(force=True)
+    if checuser(request_payload["name"]):
+        return "No such User"
+    else:
+        val = collection.find_one({"name": request_payload["name"]})["tasks"]
+
+        return json.dumps(val)
+
+"""@app.route("/markTaskComplete", methods=["POST"])
+def msrktaskcomplete():
+    request_payload = request.get_json(force=True)
+    if checuser(request_payload["name"]):
+        return "No such User"
+    else:
+"""
 
 
 
